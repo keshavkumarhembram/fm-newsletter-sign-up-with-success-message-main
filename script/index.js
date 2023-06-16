a5ff01797e8f4969591b32e050c5edda878eb056
keshav <keshavkumarhembram>
2023-06-16 16:13:47 +0530

    first commit

---
+++ script/index.js
@@ -0,0 +1,35 @@
+const button = document.getElementById("newsletter-form");
+const emailInput = document.getElementById("user-email-id");
+const errorText = document.getElementById("error-text");
+const thankYou = document.querySelector(".thankyou");
+const subscriptionPage = document.querySelector(".subscription");
+const emailElement = document.querySelector(".email");
+const dismissButton = document.getElementById("dismiss-button");
+
+button.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const submitedEmail = event.target[0].value;
+  const check = submitedEmail.match(/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/);
+  if (!check) {
+    emailInput.classList.add("error");
+    emailInput.classList.add("error-animation");
+    errorText.innerText = "Valid email is required";
+    setTimeout(() => {
+      emailInput.classList.remove("error-animation");
+    }, 1000);
+  } else {
+    subscriptionPage.style.display = "none";
+    thankYou.style.display = "block";
+    emailElement.innerText = submitedEmail;
+  }
+});
+
+emailInput.addEventListener("keypress", () => {
+  errorText.innerText = "";
+  emailInput.classList.remove("error");
+});
+
+console.log(submitedEmail);
+dismissButton.addEventListener("click", () => {
+  location.replace("../index.html");
+});
